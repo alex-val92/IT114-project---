a8fe1dde506405b67472f1420d4ad6a15c70f2a0
alex-val92 <amv92@njit.edu>
2026-04-22 06:34:26 -0400

    ServerSoocket configured , implemented while(true), thread safe placeBid method using syncronized preventing race condition, established broadcast mech

--- Milestone 2/AuctionServer.java
+++ Milestone 2/AuctionServer.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
 
-public class AuctionServer { 
+public class AuctionServer {
     private static final int Port = 5000;
     private static double currBid = 0.0;
     private static String currBidder = "No one";
@@ -11,7 +11,28 @@ public class AuctionServer {
 
     public static void main(String[] args) {
         try(ServerSocket server = new ServerSocket(Port)){
-            System.out.println("The Deal: Server Live on Port" + Port);")
-        }
+            System.out.println("The Deal: Server Live on Port" + Port);
+
+            while (true) {
+                Socket clientSocket = server.accept();
+                AuctionHandler handler = new AuctionHandler(clientSocket);
+                clients.add(handler);// Add the new client handler to the list of clients
+                new Thread(handler).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Error starting server: " + e.getMessage());
+        }//Try block throws IOException if there is an error starting the server or accepting a client connection
+    }
+}
+
+public static synchrosnized String placeBid (String bidderName, double biggestBid) {
+    if (bidAmount > currBid) {
+        currBid = biggestBid;
+        currBidder = bidderName;
+        String highestBidUp = "Current Highest Bid" + currBid + ": by " + currBidder;
+        broadcast(highestBidUp);
+        return "You're the top bidder @ $ " + biggestBid;
+    }else {
+        return "Better luck next time bid too low highet bid is $ " + currBid;
     }
 }
