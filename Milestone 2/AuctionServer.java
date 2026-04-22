5ab63938b3cbc96b743cdfc9bb76cfd012d52314
alex-val92 <amv92@njit.edu>
2026-04-22 07:07:11 -0400

    override methods in AuctionClientHandler to handle client requests and manage the auction logic. Implement functionality to receive bids, update the current highest bid, and notify clients of changes in the auction status. Ensure that the server can handle multiple clients concurrently and maintain the integrity of the auction process.

--- Milestone 2/AuctionServer.java
+++ Milestone 2/AuctionServer.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.util.*;
 import java.util.concurrent.*;
 
-public class AuctionServer {
+public class AuctionServer{ 
     private static final int Port = 5000;
     private static double currBid = 0.0;
     private static String currBidder = "No one";
