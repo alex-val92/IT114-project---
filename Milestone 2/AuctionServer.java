92e7060aca2c2be4f80e588a9ca37d369171c755
alex-val92 <amv92@njit.edu>
2026-04-22 06:07:06 -0400

    Imports and variable declarations for the AuctionServer class. The server will listen on port 5000 and keep track of the current highest bid and the bidder's name.

--- Milestone 2/AuctionServer.java
+++ Milestone 2/AuctionServer.java
@@ -1,3 +1,11 @@
-public class AuctionServer {
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class AuctionServer { 
+    private static final int Port = 5000;
+    private static double currBid = 0.0;
+    private static String currBidder = "No one";
     
 }
