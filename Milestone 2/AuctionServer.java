e2da86b1edbadae245afb55c949e2f324b529803
alex-val92 <amv92@njit.edu>
2026-04-22 06:11:51 -0400

    Registry amd main establishes connection to the RMI[Remote Method Invocation] registry and binds the AuctionServer instance to a name for clients to look up.

--- Milestone 2/AuctionServer.java
+++ Milestone 2/AuctionServer.java
@@ -7,5 +7,11 @@ public class AuctionServer {
     private static final int Port = 5000;
     private static double currBid = 0.0;
     private static String currBidder = "No one";
-    
+    public static List<AuctionClientHandler> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        try(ServerSocket server = new ServerSocket(Port)){
+            System.out.println("The Deal: Server Live on Port" + Port);")
+        }
+    }
 }
